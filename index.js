26a50e1270f41821e2f5f2ec67bf73cca2218a5f
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-30 09:13:20 +0300

    Add double loop examples

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 'use strict';
 
+//двойные циклы
+
+for(let i = 1; i <= 10; i++){
+  console.log('-------------------------');
+  for(let j = 1; j<=10; j++){
+    console.log(`${i} * ${j} = ${i*j}`);
+  }
+  console.log('-------------------------');
+}
+
+
+const arr = [];
+const subArr = [];
+for(let i = 0; i < 10; i++){
+  
+  for(let j = 0; j < 10; j++){
+    subArr[j] = j;
+  }
+
+  arr.push(subArr);
+}
+
+console.log(arr);
 
