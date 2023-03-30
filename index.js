cc1dff51d1b78ede8e02755e9960b5cddefb596e
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-30 09:52:59 +0300

    Add double loops tasks resolve

--- index.js
+++ index.js
@@ -1,26 +1,63 @@
 'use strict';
 
-//двойные циклы
-
-for(let i = 1; i <= 10; i++){
-  console.log('-------------------------');
-  for(let j = 1; j<=10; j++){
-    console.log(`${i} * ${j} = ${i*j}`);
-  }
-  console.log('-------------------------');
-}
+//двойные циклы - tasks
 
 
-const arr = [];
-const subArr = [];
-for(let i = 0; i < 10; i++){
+function drowtriangle(){
+  let str = '';
+  for(let i = 0; i < 8; i++){
+    
+    for(let j = 0; j <= i; j++){
+     str+='*';
+    }
   
-  for(let j = 0; j < 10; j++){
-    subArr[j] = j;
+    str += '\n';
+  
   }
-
-  arr.push(subArr);
+  
+  console.log(str);
 }
 
-console.log(arr);
+function emptySquare(dimention){
+  let str2 = '';
+  for(let i = 0; i < dimention; i++){
+    
+    for(let j = 0; j < dimention; j++){
+      if(i === 0 || i === dimention-1 || j === 0 || j === dimention-1) {
+        str2 += '*';
+      }else{
+        str2 += ' ';
+  
+      }
+    }
+  
+    str2 += '\n';
+  
+  }
+  console.log(str2);
+}
 
+function diagonalSquare(dimention){
+  let str2 = '';
+  for(let i = 0; i < dimention; i++){
+    
+    for(let j = 0; j < dimention; j++){
+      if(i === 0 || i === dimention-1 || j === 0 || j === dimention-1 || i === dimention-j) {
+        str2 += '*';
+      }
+      else{
+        str2 += ' ';
+      }
+    }
+  
+    str2 += '\n';
+  
+  }
+  console.log(str2);
+}
+
+
+
+emptySquare(10);
+drowtriangle();
+diagonalSquare(10);
