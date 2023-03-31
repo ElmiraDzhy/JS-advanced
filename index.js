0a599661281774d1cf2f86cdc48e5fa43ea74adf
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-31 11:13:18 +0300

    Add closure task resolve

--- index.js
+++ index.js
@@ -1,59 +1,33 @@
 'use strict';
 
-//RECURSION
+// CLOSURE
 
+function makeCounter(){
+    let counter = 0;
 
-class Friend{
-    /**
-     * 
-     * @param {String} name - Name of friend
-     * @param {Number} amountCash 
-     * @param {Friend} friend 
-     */
-    constructor(name, amountCash, friend){
-        this.name = name;
-        this.amountCash = amountCash;
-        this.friend = friend;
+    return {
+        increment (){
+            return ++counter;
+        },
+
+        decrement (){
+            return --counter;
+        },
     }
-
-    getAllCash(){
-
-        if(this.friend === null){
-            return this.amountCash;
-
-        } else if( this.friend instanceof Friend){
-
-            return this.amountCash + this.friend.getAllCash();
-        }
-
-        if(this.friend instanceof Array){
-            
-            return this.friend.reduce((accum,friend) => { 
-                return accum += friend.getAllCash() 
-            }, this.amountCash);
-            
-            // for(let i = 0; i < this.friend.length; i++){
-            //     this.amountCash += this.friend[i].getAllCash();
-            // }
-       
-        }
-        return this.amountCash;
-
-    }
-
-
 
 }
 
-const first = new Friend('Test', 110, null);
 
-const second = new Friend('Test2', 153, null);
+const {increment, decrement} = makeCounter();
 
-const third = new Friend('Test3', 100, null);
+function createAdder (n){
+    return (x) => n+=x;
+}
 
-const fourth = new Friend('Test4', 500, [third, first, second]);
+const func = createAdder(5);
 
-const fifth = new Friend('Test5', 700, fourth);
+console.log(func(10));
+console.log(func(20));
 
-console.log(fifth.getAllCash());
+
 
