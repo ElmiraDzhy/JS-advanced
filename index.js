422b8da3cba8874fa3bc866d39f3097063f67887
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-04 10:15:32 +0300

    Add algorithms realization examples

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
 'use strict';
 
-// DATE  - Standard built-in object
+// Algorithms
 
-
-function getMyBirth(){
-    const userBirth = new Date(prompt('year-month-day'));
-    const thisYearBirth = new Date(new Date().getFullYear(), userBirth.getMonth(), userBirth.getDate())
-    const msInDay = 24*60*60*1000;
-    return Math.floor((thisYearBirth.getTime() - Date.now()) / msInDay) ;
-
+const nums = [1,2,3,4,5,4,6,7,6,5,6,54,56,6,3];
+//O(n)
+const linearSearch = (elem, aar) =>{
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i] === elem) {
+            return i;
+        }
+    }
+    return -1;
 }
 
-console.log(getMyBirth());
+//O(n^2)
+
+const multyTable = (limit) =>{
+    const table = [];
+    for(let i = 1; i <= limit; i++){
+        for(let j = 1; j <= limit; j++){
+            table.push(`${i} * ${j} = ${j*i}`);
+        }
+    }
+    return table;
+}
+
+// console.log(multyTable(10));
+
+
+//Binary search - works only with sorted data
+
+const sortedArr = [1,2,3,4,5,6,7,8];
+
+//O(log(n))
+const binarySearch = (elem, arr) =>{
+    let start = 0;
+    let end = arr.length-1;
+    let middle = Math.round((start + end) / 2);
+
+    while(true){
+        if(arr[middle] === elem){
+            return middle;
+        }
+
+        if(arr[middle] > elem){
+            end = middle;
+            middle = Math.floor((start + end) / 2);
+        }
+
+        if(arr[middle] < elem){
+            start = middle;
+            middle = Math.ceil((start + end) / 2);
+        }
+    }
+
+   
+}
+
+//  console.log(binarySearch(6,sortedArr))
