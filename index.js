d71c159b8261a7528abe516d90d438df49a81f97
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-05 11:51:12 +0300

    Add task annogram solution

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
 'use strict';
 
-const obj = {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
+
+/**
+ * 
+ * @param {String} str1 
+ * @param {String} str2 
+ * @returns {Boolean}
+ */
+
+function isAnnogram(str1, str2){
+
+     const map = createVocabulary(str1.toLowerCase().split(''));
+    const map2 = createVocabulary(str2.toLowerCase().split(''));
+
+    if(map.size !== map2.size){
+        return false;
+    }
+
+
+    for (const key of map.keys()) {
+        if(map.get(key) != map2.get(key)){
+            return false;
+        }
+    }
+
+    return true;
+
+}
+    
+function createVocabulary(str){
+
+    const map = new Map();
+   
+let counter = 0;
+    for(let i = 0; i < str.length; i++){
+
+        if(map.has(str[i])){
+
+            counter = map.get(str[i]);
+            map.set(str[i], counter+1);
+        } else{
+
+            map.set(str[i], 1);
+        }
+       
+    }
+    return map;
 }
 
-const map = new Map();
-map.set('key1', 'value1');
-map.set('key2', 'value2');
-map.set('key3', 'value3');
 
 
-console.log(Object.keys(obj));
-console.log(Object.values(obj));
-console.log(Object.entries(obj));
 
-
-console.log(map.keys());
-console.log(map.values());
-console.log(map.entries());
-
-
+console.log(isAnnogram('amma', 'mama'));
+console.log(isAnnogram('amama', 'mama'));
+console.log(isAnnogram('apma', 'mama'));
+console.log(isAnnogram('hello', 'olleh'));
